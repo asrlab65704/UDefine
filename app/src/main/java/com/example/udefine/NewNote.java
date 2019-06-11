5c379caab6bdc3feca73102019d1bc300214e4b3
jkrvivian <jkrvivian@gmail.com>
2019-06-11 11:51:46 +0800

    Modify the return type of getLayoutValue to Notes list

--- app/src/main/java/com/example/udefine/NewNote.java
+++ app/src/main/java/com/example/udefine/NewNote.java
@@ -1,5 +1,6 @@
 package com.example.udefine;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -19,6 +20,11 @@ import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.example.udefine.Database.LayoutList;
+import com.example.udefine.Database.NoteList;
+import com.example.udefine.Database.Notes;
+import com.example.udefine.Database.ViewModel;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -26,6 +32,10 @@ import java.util.Map;
 
 public class NewNote extends AppCompatActivity {
     private widgetManager widgetsManager;
+    private ViewModel mViewModel;
+    private LayoutList mLayoutList;
+    private NoteList mNoteList;
+    private Notes mNotes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +44,9 @@ public class NewNote extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Create ViewModel instance
+        //mViewModel = ViewModelProviders.of(this).get(ViewModel.class);
 
         // TODO: grab layout component ID from DB
         /*
@@ -68,8 +81,18 @@ public class NewNote extends AppCompatActivity {
     }
 
     public void saveNote(View view) {
-        widgetsManager.getLayoutValue();
+        ArrayList<Notes> n = widgetsManager.getLayoutValue(0);
         // TODO: Store to DB
+        // 給 notelist  值 => note table
+        //mNoteList = new NoteList("Title", "08/06/2019,14:30", null, 1);
+        // insert note list
+        //mViewModel.insertNoteList(mNoteList);
+        // get last note id
+        //int id = mViewModel.getLastNoteID();
+        //  TODO: 一個欄位 insert 一次 => ArrayList<Notes>
+        //mNotes = new Notes(id, "title", "title");
+
+
         finish();
     }
 
