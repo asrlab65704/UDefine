79c3cd517a5eb91974eef66974c88bd2591ce083
oiz5201618 <oiz5201618@gmail.com>
2019-06-14 15:41:33 +0800

    Finish delete layout function.

--- app/src/main/java/com/example/udefine/LayoutSelection.java
+++ app/src/main/java/com/example/udefine/LayoutSelection.java
@@ -1,7 +1,10 @@
 package com.example.udefine;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,13 +12,19 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
+
+import com.example.udefine.Database.LayoutList;
+import com.example.udefine.Database.NoteList;
+import com.example.udefine.Database.ViewModel;
 
 import java.util.LinkedList;
+import java.util.List;
 
 public class LayoutSelection extends AppCompatActivity {
 
-    // TODO:testing data, should be remove later.
-    private final LinkedList<String> mLayoutList = new LinkedList<>();
+    // TODO:DB data
+    private ViewModel mViewModel;
 
     private RecyclerView mRecyclerView;
     private LayoutSelectionAdapter mAdapter;
@@ -34,29 +43,43 @@ public class LayoutSelection extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(LayoutSelection.this, NewLayout.class);
-//                String message = mMessageEditText.getText().toString();
-//                intent.putExtra(EXTRA_MESSAGE, message);
                 startActivity(intent);
             }
         });
 
-        // TODO: DB
-        for (int i = 0; i < 20; i++) {
-            mLayoutList.addLast("Layout: " + i);
-        }
+        // DB parameter initialize
+        mViewModel = ViewModelProviders.of(this).get(ViewModel.class);
+        mViewModel.getAllLayoutList().observe(this, new Observer<List<LayoutList>>() {
+            @Override
+            public void onChanged(@Nullable final List<LayoutList> layoutLists) {
+                mAdapter.setLayoutList(layoutLists);
+            }
+        });
 
         // Get a handle to the RecyclerView.
         mRecyclerView = findViewById(R.id.layout_selection_recyclerview);
         // Create an adapter and supply the data to be displayed.
-        mAdapter = new LayoutSelectionAdapter(this, mLayoutList);
+        mAdapter = new LayoutSelectionAdapter(this);
         // Connect the adapter with the RecyclerView.
         mRecyclerView.setAdapter(mAdapter);
         // Give the RecyclerView a default layout manager.
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
-    public void edit_layout(View view) {
+    public void edit_layout(View view)
+    {
         Intent intent = new Intent(LayoutSelection.this, EditLayout.class);
         startActivity(intent);
+    }
+
+    public void del_layout(View view)
+    {
+        int del_layout_id = mAdapter.get_del_layout_id();
+        if (del_layout_id != -1) {
+            mViewModel.deleteLayout(del_layout_id);
+            mAdapter.notifyDataSetChanged();
+        } else {
+            Toast.makeText(getApplicationContext(), "No layout can be deleted.", Toast.LENGTH_LONG).show();
+        }
     }
 }
